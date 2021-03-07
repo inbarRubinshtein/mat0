a5eac691ffb82b11034bd7f2bccad1ec570500ba
“inbarRubinshtein” <inbar2244@gmail.com>
2021-03-08 00:04:42 +0200

    “text”

---
+++ add.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar  7 23:58:07 2021
+
+@author: inbar
+"""
+
+first=int(input("enter first number:"))
+second=int(input("enter second number:"))
+plus = first+second
+print(plus)
